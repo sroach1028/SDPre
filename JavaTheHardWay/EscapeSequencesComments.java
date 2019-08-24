54c37832d4ff7322417ef2d0c5a7662f8a362414
Shaun Roach <sroach1028@gmail.com>
2019-08-23 17:47:32 -0700

    prework exercise 4

---
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -0,0 +1,15 @@
+public class EscapeSequencesComments {
+  public static void main(String[] args) {
+    System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n");
+    System.out.print( "\tLearn Java the \"Hard\" Way!\n");
+    System.out.print( "Hello\n");
+    System.out.print( "Jello\by\n");
+
+    System.out.println( "Hard to believe, eh");
+    System.out.println( "Surprised? Or what did you expect?");
+    System.out.println( "\\ // _=_ \\ //");
+    System.out.println( "\\\\ \\\\\\ \\\\\\\\");
+    System.out.println( "I hope you understand \"escape sequences\" now. \n");
+
+  }
+}
