6c1b5ba1c1427a70385d4137902881647fac7ddb
Shaun Roach <sroach1028@gmail.com>
2019-08-25 22:42:34 -0600

    Objective 1 Lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main (String[] args){
+    System.out.println("Name : Shaun Roach");
+    System.out.println("Age : 30");
+    System.out.println("State : Missouri");
+  }
+}
