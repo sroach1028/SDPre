3c758cff63dafeff20253d81cb76e697cf5948ec
Shaun Roach <sroach1028@gmail.com>
2019-08-25 22:54:07 -0600

    Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main (String[] args){
+    String name = "Shaun Roach";
+    int age = 30;
+    String state = "Missouri";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
