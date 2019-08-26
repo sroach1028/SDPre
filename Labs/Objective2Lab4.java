0704f4f80f74572537dd8e4b5076e351815d570c
Shaun Roach <sroach1028@gmail.com>
2019-08-25 23:10:20 -0600

    Objective 2 Lab 4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,11 @@
+public class Objective2Lab4{
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+
+    num1 = 8;
+    num2 = 13.4;
+    num3 = 12.7;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
