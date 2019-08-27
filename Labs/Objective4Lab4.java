b40b0abb50a4fe02f8a661a2f7645c56a5aac291
Shaun Roach <sroach1028@gmail.com>
2019-08-27 11:25:29 -0600

    Objective 4 Lab 4

---
+++ Labs/Objective4Lab4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Objective4Lab4 {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+
+    System.out.print("Please enter a number: ");
+    userNum = scanner.nextInt();
+
+    if ((userNum % 2) == 0){
+      System.out.println("The number is even");
+    }
+    else {
+      System.out.println("The number is odd");
+    }
+  }
+}
