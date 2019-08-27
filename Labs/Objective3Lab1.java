4870dbc63e43a36b4bc11b2e632e5d4644050ee4
Shaun Roach <sroach1028@gmail.com>
2019-08-26 21:11:57 -0600

    Objective 3 Lab 1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Objective3Lab1 {
+public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("Please type a word: ");
+  String userInput = input.next();
+
+  System.out.println(userInput);
+}
+}
