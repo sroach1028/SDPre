fb77b9ffd378d5741e24d124b82956fc4fd7a07f
Shaun Roach <sroach1028@gmail.com>
2019-08-26 21:44:03 -0600

    Objective 3 Lab 2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Objective3Lab2 {
+  public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("What's your name?");
+  String name = input.next();
+
+  System.out.println("Hello " + name);  
+  }
+}
